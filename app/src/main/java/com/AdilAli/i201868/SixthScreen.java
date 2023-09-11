1d6369cf5226acaaa36be582c693696e43d68a39
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2023-09-11 22:12:23 +0500

    Layout of Fifth Screen

---
+++ app/src/main/java/com/AdilAli/i201868/SixthScreen.java
@@ -0,0 +1,14 @@
+package com.AdilAli.i201868;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SixthScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sixth_screen);
+    }
+}
