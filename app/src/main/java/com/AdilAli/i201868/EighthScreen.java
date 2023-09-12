c9a8f179ba3e4012de1554ca0645ef8c81e51de0
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2023-09-12 23:09:06 +0500

    Layout of Eighth Screen

---
+++ app/src/main/java/com/AdilAli/i201868/EighthScreen.java
@@ -0,0 +1,14 @@
+package com.AdilAli.i201868;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EighthScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eighth_screen);
+    }
+}
