9bbdb6642eb999146453eb48c9ae9181952f9283
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2023-09-12 11:21:21 +0500

    Layout of Seventh Screen

---
+++ app/src/main/java/com/AdilAli/i201868/SeventhScreen.java
@@ -0,0 +1,14 @@
+package com.AdilAli.i201868;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeventhScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seventh_screen);
+    }
+}
