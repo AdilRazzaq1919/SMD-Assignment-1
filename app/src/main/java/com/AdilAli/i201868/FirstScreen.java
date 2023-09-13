f66ff491ce2b49ffe30885239223b09c2762ca72
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2023-09-13 23:54:56 +0500

    Layout of Ninth Screen

--- app/src/main/java/com/AdilAli/i201868/FirstScreen.java
+++ app/src/main/java/com/AdilAli/i201868/FirstScreen.java
@@ -2,13 +2,37 @@ package com.AdilAli.i201868;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FirstScreen extends AppCompatActivity {
-
+    TextView signup;
+    TextView forgetPassword;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+        signup=findViewById(R.id.signup);
+        forgetPassword=findViewById(R.id.forgotPassword);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstScreen.this,ThirdScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstScreen.this, SecondScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
