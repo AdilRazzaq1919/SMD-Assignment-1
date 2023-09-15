032f25b55ce7fcad3fcae91a176b8a40036cf853
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2023-09-15 12:42:26 +0500

    Layout of Seventeen and Ninteen Screen

---
+++ app/src/main/java/com/AdilAli/i201868/NinteenScreen.java
@@ -0,0 +1,14 @@
+package com.AdilAli.i201868;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NinteenScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ninteen_screen);
+    }
+}
---
+++ app/src/main/java/com/AdilAli/i201868/SeventeenScreen.java
@@ -0,0 +1,14 @@
+package com.AdilAli.i201868;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeventeenScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seventeen_screen);
+    }
+}
